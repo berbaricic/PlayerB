444db3bc36c39a49f55b4b0756f4cb867d1c3ea1
Berislav Baricic <berislavbaricic95@gmail.com>
2020-08-06 15:02:15 +0200

    Izmjena konekcije za SQL

--- init/Script.sql
+++ init/Script.sql
@@ -1,10 +1,4 @@
-CREATE DATABASE SessionDatabase;
-USE SessionDatabase;
-
-CREATE TABLE Session (
-	Id varchar(50) PRIMARY KEY NOT NULL,
-	Status varchar(50) NOT NULL,
-	UserAdress varchar(50) NULL,
-	IdVideo varchar(50) NOT NULL,
-	RequestTime int NOT NULL
-);
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name='SessionDatabase')
+    CREATE DATABASE [SessionDatabase]
+GO
+USE [dba]
